02df50c425793553aea9e341b2695dfd84a98d84
PHIL KIM <philsgu@gmail.com>
2024-04-13 17:10:00 -0700

    updated html

--- mysite/urls.py
+++ mysite/urls.py
@@ -14,8 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include #add include for personal blog url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('blog.urls')) #initial IP address:8000 port wil now look for this file
 ]
